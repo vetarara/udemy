6ea6b8ad4624efc86ebd9af7336442d496b8282a
Veta <vetarara@yandex.ru>
2026-08-18 00:00:41 +0300

    2.18

---
+++ index.js
@@ -0,0 +1,21 @@
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
+
+let a = prompt("Один из последних просмотренных фильмов?");
+let b = +prompt("На сколько оцените его?", '');
+let c = prompt("Один из последних просмотренных фильмов?");
+let d = +prompt("На сколько оцените его?", '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
